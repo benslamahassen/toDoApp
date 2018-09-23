c65a24b310aec6b1ae7d75de3498af8b1dae510b
benslamahassen <benslamahassen5@gmail.com>
2018-09-23 11:14:39 +0100

    Implemeting async/await & Try/Catch.

--- lib/controllers/controller.ts
+++ lib/controllers/controller.ts
@@ -4,52 +4,47 @@ import { Request, Response } from 'express';
 
 const Task = mongoose.model('Task', TaskSchema);
 export class TaskController {
-  public addNewTask(req: Request, res: Response) {
-    let newTask = new Task(req.body);
-
-    newTask.save((err, task) => {
-      if (err) {
-        res.send(err);
-      }
+  public async addNewTask(req: Request, res: Response) {
+    const newTask = new Task(req.body);
+    try {
+      const task = await newTask.save();
       res.json(task);
-    });
+    } catch (err) {
+      res.json(err);
+    }
   }
-  public getTasks(req: Request, res: Response) {
-    Task.find({}, (err, task) => {
-      if (err) {
-        res.send(err);
-      }
+  public async getTasks(req: Request, res: Response) {
+    try {
+      const result = await Task.find();
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  public async getTaskWithID(req: Request, res: Response) {
+    try {
+      const task = await Task.findById(req.params.contactId);
       res.json(task);
-    });
+    } catch (err) {
+      res.json(err);
+    }
   }
-  public getTaskWithID(req: Request, res: Response) {
-    Task.findById(req.params.contactId, (err, task) => {
-      if (err) {
-        res.send(err);
-      }
+  public async updateTask(req: Request, res: Response) {
+    try {
+      const task = await Task.findOneAndUpdate({ _id: req.params.taskId }, req.body, {
+        new: true
+      });
       res.json(task);
-    });
+    } catch (err) {
+      res.json(err);
+    }
   }
-  public updateTask(req: Request, res: Response) {
-    Task.findOneAndUpdate(
-      { _id: req.params.taskId },
-      req.body,
-      { new: true },
-      (err, task) => {
-        if (err) {
-          res.send(err);
-        }
-        res.json(task);
-      }
-    );
-  }
-  public deleteTask(req: Request, res: Response) {
-    Task.deleteOne({ _id: req.params.taskId }, (err, task) => {
-      if (err) {
-				res.send(err);
-				console.log(err)
-      }
-      res.json({ success:true });
-    });
+  public async deleteTask(req: Request, res: Response) {
+    try {
+      await Task.deleteOne({ _id: req.params.taskId });
+      res.json({ success: true });
+    } catch (err) {
+      res.json(err);
+    }
   }
 }
